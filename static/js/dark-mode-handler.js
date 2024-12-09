0a58d94ab2831d33f0e1e44af9478798225aef4e
Amir1 <amir.gabbai8@gmail.com>
2024-12-09 16:10:38 +0200

    added close-open section

--- static/js/dark-mode-handler.js
+++ static/js/dark-mode-handler.js
@@ -9,13 +9,11 @@ function styleModeToggle() {
         imageElement.src = 'static/icons/dark-mode-icon.svg';
     }
 
-    // Add event listener to toggle dark mode on button click
-    toggleButton.addEventListener('click', () => {
-        const isDarkMode = document.body.classList.toggle('dark-mode'); // Toggle the dark-mode class
-        if (isDarkMode) {
-            imageElement.src = 'static/icons/dark-mode-icon.svg'; // Set icon for light mode toggle
-        } else {
-            imageElement.src = 'static/icons/light-mode-icon.svg'; // Set icon for dark mode toggle
-        }
-});
+   
+    const isDarkMode = document.body.classList.toggle('dark-mode'); // Toggle the dark-mode class
+    if (isDarkMode) {
+        imageElement.src = 'static/icons/dark-mode-icon.svg'; // Set icon for light mode toggle
+    } else {
+        imageElement.src = 'static/icons/light-mode-icon.svg'; // Set icon for dark mode toggle
+    }
 }
